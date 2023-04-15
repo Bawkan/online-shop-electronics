9a94c0669029a3e2099cb6226f48770c721c50c6
Chingiz Nasibbeyli <reis_969@mail.ru>
2023-04-15 09:56:36 +0300

    Update tests.py

--- work_in_django/orders/tests.py
+++ work_in_django/orders/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .forms import OrderForm
 
-# Create your tests here.
+
+class OrderValidTestCase(TestCase):
+
+    def test_order(self):
+
+        data = {'full_name': 'Ivanov',
+                'phone': '890',
+                'email': 'test@mail.ru',
+                'city': 'Moscow',
+                'address': 'prospect mira'
+                }
+
+        for form in data:
+            form = OrderForm(data=data)
+            self.assertTrue(form)
+            
